cf96c880c85acb59f27a004f84a4231cfcbcbad0
Skeleton Army <skeleton.army23644@gmail.com>
2026-06-04 18:35:51 +0300

    feat: add pp constants and follower

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonomousApp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonomousApp.java
@@ -1,19 +1,26 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.pedropathing.follower.Follower;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.skeletonarmy.marrow.LynxUtil;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
 @Autonomous(name="Autonomous", preselectTeleOp="TeleOpApp")
 public class AutonomousApp extends CommandOpMode {
+    private Follower follower;
+
     @Override
     public void initialize() {
         LynxUtil.setBulkCachingMode(hardwareMap, LynxModule.BulkCachingMode.AUTO);
+
+        follower = Constants.createFollower(hardwareMap);
     }
 
     @Override
     public void run() {
-
+        follower.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpApp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpApp.java
@@ -1,19 +1,27 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.pedropathing.follower.Follower;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
 import com.skeletonarmy.marrow.LynxUtil;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
 @TeleOp(name="TeleOp")
 public class TeleOpApp extends CommandOpMode {
+    private Follower follower;
+
     @Override
     public void initialize() {
         LynxUtil.setBulkCachingMode(hardwareMap, LynxModule.BulkCachingMode.AUTO);
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.startTeleOpDrive(true);
     }
 
     @Override
     public void run() {
-
+        follower.update();
     }
 }
